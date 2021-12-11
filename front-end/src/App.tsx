1bfe54697586344b7a973a82a3e14dd7ef84d42b
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-12-11 15:06:58 -0300

    :heavy_plus_sign: Adiciona dependência do cors para permição de uso do servidor para outros endereços
    - Adicionado uma nova cor ao arquivo de estilo global

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+
 import { 
   Title, 
   Select 
 } from './components';
 
+import { DataProps } from './@types';
+
 import styles from './style/home.module.scss';
 
+async function getClimateData(data: DataProps[]) {
+  const response = await fetch("http://localhost:4444/api/getClimateAll");
+
+  console.log(response);
+}
+
 export default function App() {
+  let [ data, setData ] = useState([]);
+
+  // getClimateData(data);
+
   const { main } = styles;
 
   return (
