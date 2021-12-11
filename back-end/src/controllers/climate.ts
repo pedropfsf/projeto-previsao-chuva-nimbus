efb319a7cc87eaa181c510bca2a194818d71a195
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-12-10 21:19:05 -0300

    :sparkles: Feito tratamento de error nas variáveis de bairro e dias de chuva

--- back-end/src/controllers/climate.ts
+++ back-end/src/controllers/climate.ts
@@ -17,15 +17,49 @@ export const createDataClimate = (request, response) => {
         days 
     }:climateProps = request.body;
 
-    const ID = createID();
+    const isDistrictIsNumber = typeof district === "number";
+    const isDaysIsArray = Array.isArray(days);
 
-    const newData = {
-        id: ID,
-        district,
-        days
-    };
+    function responseSendError(message:string) {
+        response.status(406).send(`
+            <div style="font-family: Arial">
+                <h1
+                    style="
+                        color: #aa0022;
+                        font-weight: bold;
+                        text-align: center;
+                        font-size: 60px
+                    "
+                >
+                    Error 406
+                </h1>
+                <p 
+                    style="
+                        font-size: 30px;
+                        text-align: center;
+                    "
+                >
+                    ${ message }
+                </p>
+            </div>
+        `);
+    }
 
-    data.climateArray.push(newData);
+    if(isDistrictIsNumber) {
+        responseSendError("O bairro que você está tentando enviar é do tipo invalido, somente é permitido valor do bairro do tipo string(Texto) e não number(Número)");
+    } else if(!isDaysIsArray) {
+        responseSendError("Os dados de dia de chuva que você está tentando enviar não é do tipo array");
+    } else {
+        const ID = createID();
+    
+        const newData = {
+            id: ID,
+            district,
+            days
+        };
+    
+        data.climateArray.push(newData);
 
-    response.send("Salvado com sucesso");
+        response.send("Salvo com sucesso");
+    }
 }
